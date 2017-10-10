9f00eab287ae3d5de8d8870fcde1bd40004f9c57
Noah Horlacher (KUROMARU) <kuromarumusic@gmail.com>
2017-10-10 03:46:00 +0200

    Beautified the website.

--- js/header.js
+++ js/header.js
@@ -1,11 +1,25 @@
+if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+          document.getElementById('backtotop').className = "notontop";
+} else {
+          document.getElementById('backtotop').className = "";
+}
+
 window.onscroll = function() {
           headertop()
 };
 
 function headertop() {
           if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-                    document.getElementById('navigation').className = "notontop";
+                    document.getElementById('backtotop').className = "notontop";
           } else {
-                    document.getElementById('navigation').className = "";
+                    document.getElementById('backtotop').className = "";
           }
+}
+
+document.getElementById('backtotop').onclick = function() {
+          window.scroll({
+                    top: 0,
+                    left: 0,
+                    behavior: 'smooth'
+          });
 }
