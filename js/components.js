b6850d33bd02b86ac4309f3eefaf01b39b94f8d1
Noah Horlacher <kuromarumusic@gmail.com>
2021-03-19 13:23:00 +0100

    pugified

---
+++ js/components.js
@@ -0,0 +1,40 @@
+//disable image drag and drop
+for (let e of document.querySelectorAll('img')) e.ondragstart = () => false
+
+//buttons
+document.querySelectorAll('btn').forEach(
+	(e) =>
+		(e.onclick = () => {
+			if (e.hasAttribute('link')) window.location = e.getAttribute('link')
+		})
+)
+
+//accordions
+let accordions = document.querySelectorAll('accordion')
+let lastActiveAccElt = []
+
+accordions.forEach((e, i) => {
+	e.setAttribute('index', i)
+	lastActiveAccElt.push(null)
+
+	e.querySelectorAll('item').forEach((f, j) => {
+		f.querySelector('btn:first-of-type').onclick = () => {
+			if (lastActiveAccElt[i] != j) {
+				if (f.classList.contains('active')) {
+					f.classList.remove('active')
+				} else {
+					f.classList.add('active')
+				}
+
+				lastActiveAccElt[i] = j
+			} else {
+				if (f.classList.contains('active')) {
+					f.classList.remove('active')
+				} else {
+					f.classList.add('active')
+				}
+				lastActiveAccElt[i] = null
+			}
+		}
+	})
+})
--- js/timeline.js
+++ js/timeline.js
@@ -1,30 +1,26 @@
-(function() {
+// check if an element is in viewport
+const inview = (el) => {
+	var rect = el.getBoundingClientRect()
+	return (
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		rect.bottom <=
+			(window.innerHeight || document.documentElement.clientHeight) &&
+		rect.right <=
+			(window.innerWidth || document.documentElement.clientWidth)
+	)
+}
 
-    'use strict';
-  
-    // check if an element is in viewport
-    function isElementInViewport(el) {
-      var rect = el.getBoundingClientRect();
-      return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-      );
-    }
-  
-    function callbackFunc() {
-      $("timeline li").each(function(){
-        if (isElementInViewport(this)) {
-          $(this).addClass("in-view");
-        } else {
-          //$(this).removeClass("in-view");
-        }
-      });
-    }
-  
-    // listen for events
-    window.addEventListener("load", callbackFunc);
-    window.addEventListener("resize", callbackFunc);
-    window.addEventListener("scroll", callbackFunc);
-  })();
+// add visible class if visible (don't remove if invisible)
+const checkvisible = () => {
+	document.querySelectorAll('timeline ul>li:not(.visible)').forEach((e) => {
+		if (inview(e)) e.classList.add('visible')
+	})
+}
+
+// listen for events
+;['load', 'resize', 'scroll'].forEach((e) =>
+	window.addEventListener(e, checkvisible)
+)
+
+checkvisible()
