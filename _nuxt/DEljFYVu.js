d40481c46204eec35013a11b9a19f39aea02c083
Noah Horlacher <kuromarumusic@gmail.com>
2024-10-20 14:28:18 +0200

    added quotes

---
+++ _nuxt/DEljFYVu.js
@@ -0,0 +1,12 @@
+import{c as h,w as r,o as n,a as t,f as i,e as a,b as o,r as m,u as _,F as g,i as x,t as c,_ as p}from"./CeYTXtQY.js";import{_ as f}from"./PLBID2Ue.js";const w=[{source:"Leonhard Mondsturm — das Herz des Waldes",text:`Wir Wanderer im Nebelmeer,
+fühlen diesen Nebel schwer,
+mittendrin ein Andrer steht,
+ich frage mich, wohin er geht.
+
+Durch Schleier grau und dichten Dunst,
+suchen wir nach Weg und Kunst,
+im Zwielicht zwischen Sein und Schein,
+sind wir verloren, doch nicht allein.`},{source:"Leonhard Mondsturm — das Herz des Waldes",text:"Es gibt keinen Grund, traurig zu sein, und keinen Grund, glücklich zu sein. Die menschliche Existenz basiert auf der Abhängigkeit zu den anderen. Das Dasein einzelner Menschen ist unwichtig. Die Gemeinschaft formt einen eigenen Organismus, und du bist kein Teil davon. Du bist anders genug, um nicht dazuzugehören und nicht anders genug, etwas daraus zu machen. Dein Leben wird von Einsamkeit durchzogen sein. Du wirst niemals wahre Nähe spüren."},{source:"Leonhard Mondsturm — das Herz des Waldes",text:`Dein Fleisch wird modern, dein Atem stockt,
+Wo dich der Schatten des Waldes lockt.
+Dein Herz verstummt, dein Geist verirrt,
+Der tote Wald dich nie verliert.`}],b={class:"text-center mb-32 mt-4"},z={class:"flex gap-8 gap-y-12 flex-wrap justify-center text-center"},k={class:"max-w-[400px]"},D={class:"whitespace-pre-line italic text-lg"},W={class:"mt-6"},v={class:""},N={class:"italic text-sm text-gray-600"},q={__name:"quotes",setup(y){return(H,e)=>{const d=x,u=p,l=f;return n(),h(l,{class:"bg-gray-100"},{default:r(()=>[e[3]||(e[3]=t("h2",{class:"text-4xl font-bold text-center"},"Zitate",-1)),t("p",b,[e[1]||(e[1]=i("Jetzt Zitat hinzufügen auf ")),a(d,{class:"underline",to:"https://www.goodreads.com/author/quotes/52548579.Noah_Horlacher"},{default:r(()=>e[0]||(e[0]=[i("goodreads.com")])),_:1})]),t("div",z,[(n(!0),o(g,null,m(_(w),s=>(n(),o("figure",k,[a(u,{name:"tabler:quote",class:"w-14 h-14 text-gray-500"}),t("blockquote",D," «"+c(s.text)+"» ",1),t("figcaption",W,[t("div",v,[e[2]||(e[2]=t("cite",{class:"italic text-md pr-4 mr-4 border-r-[1px] border-black"},"Noah Horlacher",-1)),t("cite",N,c(s.source),1)])])]))),256))])]),_:1})}}};export{q as default};
