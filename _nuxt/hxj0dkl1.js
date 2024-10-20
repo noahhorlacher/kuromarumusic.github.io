ff74d77441f94f7b84ba5c834247ebdec5211fba
Noah Horlacher <kuromarumusic@gmail.com>
2024-10-20 08:48:48 +0200

    new version

---
+++ _nuxt/hxj0dkl1.js
@@ -0,0 +1,1 @@
+import{_ as s,o,c as t,a as c,j as a}from"./DMqPbVYN.js";const n={},_={class:"p-32"},r={class:"max-w-[960px] mx-auto"};function d(e,l){return o(),t("section",_,[c("div",r,[a(e.$slots,"default")])])}const m=s(n,[["render",d]]);export{m as _};
