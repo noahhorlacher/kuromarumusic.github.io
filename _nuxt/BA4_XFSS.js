08d43899f257db011bac79f05a6005324202e904
Noah Horlacher <kuromarumusic@gmail.com>
2024-10-22 15:01:17 +0200

    added new cover

--- _nuxt/BA4_XFSS.js
+++ _nuxt/BA4_XFSS.js
@@ -1,4 +1,4 @@
-import{d as h,w as a,g as s,f as e,i as o,b as d,c as r,h as m,a as _,F as g,k as x,t as i,e as p,j as k}from"./BYGOWxRM.js";import{_ as f}from"./tMVwkzEi.js";const w=[{source:"Leonhard Mondsturm — das Herz des Waldes",speaker:"Lied des Schicksals",text:`Wir Wanderer im Nebelmeer,
+import{d as h,w as a,g as s,f as e,i as o,b as d,c as r,h as m,a as _,F as g,k as x,t as i,e as p,j as k}from"./CgfqZb2O.js";import{_ as f}from"./EZ4zIOYW.js";const w=[{source:"Leonhard Mondsturm — das Herz des Waldes",speaker:"Lied des Schicksals",text:`Wir Wanderer im Nebelmeer,
 fühlen diesen Nebel schwer,
 mittendrin ein Andrer steht,
 ich frage mich, wohin er geht.
