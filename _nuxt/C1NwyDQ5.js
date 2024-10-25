1021ba5ebc74d30374267f957ddfd6539f247f15
Noah Horlacher <kuromarumusic@gmail.com>
2024-10-25 13:43:38 +0200

    added Orell Füssli dropdown

--- _nuxt/C1NwyDQ5.js
+++ _nuxt/C1NwyDQ5.js
@@ -1,4 +1,4 @@
-import{d as h,w as a,g as s,f as e,i as o,b as d,c as r,h as m,a as _,F as g,k as x,t as i,e as p,j as k}from"./BqH9pSBl.js";import{_ as f}from"./Dk5K5-cK.js";const w=[{source:"Leonhard Mondsturm — das Herz des Waldes",speaker:"Lied des Schicksals",text:`Wir Wanderer im Nebelmeer,
+import{c as h,w as a,o as s,a as e,m as o,b as d,d as r,e as m,j as _,F as g,f as x,t as i,k as p,_ as k}from"./CrcIC6du.js";import{_ as f}from"./DiyE_6EO.js";const w=[{source:"Leonhard Mondsturm — das Herz des Waldes",speaker:"Lied des Schicksals",text:`Wir Wanderer im Nebelmeer,
 fühlen diesen Nebel schwer,
 mittendrin ein Andrer steht,
 ich frage mich, wohin er geht.
